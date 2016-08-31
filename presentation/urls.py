61ace15b74f1c2d447ace5cd05ff10d0ece7c16b
Thykof <nathan.seva@outlook.fr>
2016-08-31 13:44:37 +0200

    Improve navbar of presentation
    + acces page

--- presentation/urls.py
+++ presentation/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from presentation import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^description$', views.description, name='description'),
+    url(r'^$', TemplateView.as_view(template_name='presentation/home.html'), name='home'),
+    url(r'^description$', TemplateView.as_view(template_name='presentation/description.html'), name='description'),
+    url(r'^acces$', TemplateView.as_view(template_name='presentation/entrance.html'), name='entrance'),
 ]
--- presentation/views.py
+++ presentation/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-def home(request):
-    return render(request, 'presentation/home.html', locals())
-
-def description(request):
-    return render(request, 'presentation/description.html', locals())
